2887b13b72eee9510c76e198bdbfd23471a435d5
mewmew <rnd0x00@gmail.com>
2014-08-05 23:20:57 +0200

    meta: Add StreamInfo definition.

--- meta/streaminfo.go
+++ meta/streaminfo.go
@@ -1,7 +1,30 @@
 package meta
 
+import "crypto/md5"
+
 // StreamInfo contains information about the FLAC audio stream. It must be
 // present as the first metadata block of a FLAC stream.
 //
 // ref: https://www.xiph.org/flac/format.html#metadata_block_streaminfo
-type StreamInfo struct{}
+type StreamInfo struct {
+	// Minimum block size (in samples) used in the stream.
+	BlockSizeMin uint16
+	// Maximum block size (in samples) used in the stream.
+	BlockSizeMax uint16
+	// Minimum frame size in bytes; a 0 value implies unknown.
+	FrameSizeMin uint32
+	// Maximum frame size in bytes; a 0 value implies unknown.
+	FrameSizeMax uint32
+	// Sample rate in Hz; between 1 and 655350 Hz.
+	SampleRate uint32
+	// Number of channels; between 1 and 8 channels.
+	NChannels uint8
+	// Sample size in bits-per-sample; between 4 and 32 bits.
+	BitsPerSample uint8
+	// Total number of inter-channel samples in the stream. One second of 44.1
+	// KHz audio will have 44100 samples regardless of the number of channels. A
+	// 0 value implies unknown.
+	NSamples uint64
+	// MD5 checksum of the unencoded audio data.
+	MD5sum [md5.BlockSize]uint8
+}
