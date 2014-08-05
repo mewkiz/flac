ea25c67fa8e326b7cda74ea641cba341762d6daa
mewmew <rnd0x00@gmail.com>
2014-08-05 23:23:37 +0200

    meta: Add Application definition.

--- meta/application.go
+++ meta/application.go
@@ -3,4 +3,11 @@ package meta
 // Application contains application specific data.
 //
 // ref: https://www.xiph.org/flac/format.html#metadata_block_application
-type Application struct{}
+type Application struct {
+	// Registered application ID.
+	//
+	// ref: https://www.xiph.org/flac/id.html
+	ID uint32
+	// Application data.
+	Data []byte
+}
