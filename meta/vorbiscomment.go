db1994202e769d6a4fc638c43e34d539d0b0443f
mewmew <rnd0x00@gmail.com>
2014-08-06 00:48:24 +0200

    meta: Simplify VorbisComment definition.

--- meta/vorbiscomment.go
+++ meta/vorbiscomment.go
@@ -1,19 +1,11 @@
 package meta
 
-// VorbisComment contains a list of name/value-pairs.
+// VorbisComment contains a list of name-value pairs.
 //
 // ref: https://www.xiph.org/flac/format.html#metadata_block_vorbis_comment
 type VorbisComment struct {
 	// Vendor name.
 	Vendor string
-	// Name-value pair tags.
-	Tags []VorbisTag
-}
-
-// A VorbisTag represents a name/value-pair.
-type VorbisTag struct {
-	// Tag name.
-	Name string
-	// Tag value.
-	Val string
+	// A list of tags, each represented by a name-value pair.
+	Tags [][2]string
 }
